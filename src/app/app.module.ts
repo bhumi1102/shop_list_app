43176e593add5aa0f6ba115f6b151bde5f238e29
Bhumi Shah <bhumi1102@gmail.com>
2016-12-10 23:44:54 -0800

    add realtime sync with firebase for store list view and creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { MyApp } from './app.component';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
 import { StoreListPagePage } from '../pages/store-list-page/store-list-page';
 import { StoreDetailsPage } from '../pages/store-details/store-details';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD5p5sy4ACcjimX6sPwARrKCDw1aF5rRic",
+  authDomain: "shop-list-8f175.firebaseapp.com",
+  databaseURL: "https://shop-list-8f175.firebaseio.com",
+  storageBucket: "shop-list-8f175.appspot.com",
+  messagingSenderId: "663897269423"
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +28,9 @@ import { StoreDetailsPage } from '../pages/store-details/store-details';
     StoreDetailsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
