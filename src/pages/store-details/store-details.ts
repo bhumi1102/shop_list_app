7ed47031b7753f40f2dee8b78894f462ca13cc16
Bhumi Shah <bhumi1102@gmail.com>
2016-12-14 23:13:05 -0800

    navigate to store details page and list items on the page, with new schema model

--- src/pages/store-details/store-details.ts
+++ src/pages/store-details/store-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 /*
   Generated class for the StoreDetails page.
@@ -13,10 +14,24 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class StoreDetailsPage {
   selectedItem: any;
+  items: FirebaseListObservable<any[]>;
+  itemName: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              af: AngularFire) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
+    var url = '/commonStores/'+this.selectedItem;
+    console.log(url);
+    this.items = af.database.list(url);
+    console.log(this.items);
+  }
+
+  createStore($event, item) {
+    console.log('name of new store to persist: ' + this.itemName);
+
+    this.items.push(this.itemName);
   }
 
   ionViewDidLoad() {
